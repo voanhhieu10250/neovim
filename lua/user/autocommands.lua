75539055bd00d54841d1e6167ed7bce603ee1612
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-07-07 12:36:50 +0700

    hide buf tabline by default

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -40,14 +40,14 @@
 
 
 
-vim.api.nvim_create_autocmd({ "User" }, {
-  pattern = { "AlphaReady" },
-  callback = function()
-    vim.cmd [[
-      set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-    ]]
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "User" }, {
+--   pattern = { "AlphaReady" },
+--   callback = function()
+--     vim.cmd [[
+--       set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+--     ]]
+--   end,
+-- })
 
 vim.api.nvim_create_autocmd({ "User" }, {
   pattern = { "AlphaReady" },
