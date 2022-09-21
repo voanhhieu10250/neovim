81173bc4b01c266f084accbcfafcfdcf789536d7
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-09-21 12:22:50 +0700

    auto detect wsl and config g:clipboard

--- lua/user/clipboard.lua
+++ lua/user/clipboard.lua
@@ -1,13 +1,15 @@
 -- This is for WSL on windows. 
--- If you're using Unix base OS please comment all of the code below
-vim.g.clipboard = {
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf",
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --lf",
-    ["*"] = "win32yank.exe -o --lf",
-  },
-}
+
+if vim.fn.has('wsl') == 1 then
+  vim.g.clipboard = {
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+  }
+end
 
