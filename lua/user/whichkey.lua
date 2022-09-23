eff2e5caea9500d4ef00541fbbc97af0c9d4508a
Vo Anh Hieu <voanhhieu10250@gmail.com>
2022-09-23 20:56:28 +0700

    update

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -186,11 +186,7 @@ local mappings = {
 		name = "LSP",
 		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
     c = { "<cmd>lua require('user.lsp').server_capabilities()<cr>", "Get Capabilities" },
-		d = { "<cmd>TroubleToggle<cr>", "Diagnostics" },
-		w = {
-			"<cmd>Telescope lsp_workspace_diagnostics<cr>",
-			"Workspace Diagnostics",
-		},
+		-- d = { "<cmd>TroubleToggle<cr>", "Diagnostics" },
 		f = { "<cmd>lua vim.lsp.buf.format({async = true})<cr>", "Format" },
 		F = { "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat" },
 		i = { "<cmd>LspInfo<cr>", "Info" },
@@ -210,7 +206,7 @@ local mappings = {
 		o = { "<cmd>SymbolsOutline<cr>", "Outline" },
 		q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
 		r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-		R = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
+		-- R = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
 		s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
 		S = {
 			"<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
